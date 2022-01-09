dbf67cf3caec01c030780bf98c0976423cdbdb8b
angel7170 <58609234+angel7170@users.noreply.github.com>
2022-01-09 10:20:03 -0500

    Add files via upload

---
+++ Desk.js
@@ -0,0 +1,52 @@
+function navigate()
+{
+    window.location = "index.html";
+}
+
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
+}
+
+ img = "";
+ status1 = "";
+
+ function preload()
+{
+    img = loadImage("Desk.jpg");
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Desk", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded!");
+    status1 = true;
+     objectDetector.detect(img , gotResults);
+}
+
+function gotResults(error, results)
+{
+   if(error)
+   {
+       console.log(error);
+   }
+
+   else
+   {
+       console.log(results);
+   }
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function navigate1()
+{
+    window.location = "bedroom.html";
+}
+
+function navigate2()
+{
+    window.location = "TV&AC.html";
+}
+
+function navigate3()
+{
+    window.location = "Desk.html";
+}
+
+function navigate4()
+{
+    window.location = "livingroom.html";
+}
+
+function navigate5()
+{
+    window.location = "bottle.html";
+}
